1b0fa32a06c85b07a37c73447be2a60091d709d0
Shubham <shubhamtripathigis32@gmail.com>
2022-05-16 17:50:58 +0530

    shubh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { AppComponent } from './app.component';
 import { OrderListComponent, ProductListComponent, UserListComponent, ProductPageComponent } from './components';
 import { OrdersContainerComponent, ProductsContainerComponent, UsersContainerComponent } from './containers';
 import { CategoryPipe, ColorPipe } from './pipes';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule, Ng2OrderPipe } from 'ng2-order-pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,11 +23,16 @@ import { CategoryPipe, ColorPipe } from './pipes';
     ProductPageComponent,
     CategoryPipe,
     ColorPipe,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule,FormsModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from 'src/app/services';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,7 +9,13 @@ import { ProductService } from 'src/app/services';
 })
 export class ProductListComponent{
 
+  categories:string[]=["Sneaker","Casual","Athletic"];
+  brands:string[]=["Nike","Addidas","PUMA"]
   products: any;
+  data='';
+  productCreated:any;
+  productEdit: any;
+  filterTerm!: any;
 
   constructor(private product: ProductService) {
     this.get();
@@ -16,6 +23,20 @@ export class ProductListComponent{
 
   get(){
     this.product.getProducts().subscribe(data => this.products = data);
+  }
+  OnFormSubmit(b:string,n:string,d:string,p:string,ct:string,s:string,cl:string,q:string,i:string){
+    this.data='{"brand":"'+b+'","name":"'+n+'","description":"'+d+'","price":'+p+',"category":"'+ct+'","size":'+s+',"color":"'+cl+'","quantity":'+q+',"image":"'+i+'"}';
+    this.product.setProduct(this.data).subscribe(a=>this.productCreated=a);
+    location.reload();
+  }
+  getById(){
+    this.productEdit =  this.products;
+  }
+  key:string='brand';
+  reverse :boolean =false;
+  sort(key:any){
+    this.key=key;
+    this.reverse=!this.reverse;
   }
 
 }
